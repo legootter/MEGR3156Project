2e0b7bd406a96aa5ce1a71e93ad753acb4819ce9
legootter <legootter@gmail.com>
2023-01-30 10:41:23 -0500

    added comments to the code

--- code/MEGR3156Robot.ino
+++ code/MEGR3156Robot.ino
@@ -1,31 +1,35 @@
-#include <PS2X_lib.h>
-#include <Servo.h>
-#include "drivetrain.cpp"
-#define DAT A0
-#define CMD A1
-#define SEL A2
-#define CLK A3
-int RYPOS=0;
-int LYPOS=0;
-bool triggers;
-PS2X ps2x;
-drivetrain motors;
+/*MEGR 3156 robot code
+Group 3: Alex Norris, others..
+
+*/
+#include <PS2X_lib.h>   //including the library for connecting to the ps2 controller
+#include <Servo.h>      //including the servo library
+#include "drivetrain.cpp"   //including the motors class for the robot
+#define DAT A0          //defining the data pin for the ps2 controller, pin 1
+#define CMD A1          //defining the command pin for the ps2 controller, pin 2
+#define SEL A2          //defining the attention pin for the ps2 controller, pin 6
+#define CLK A3          //defining the clock pin for the ps2 controller, pin 7
+int RYPOS=0;            //position (0-255) of the right joystick y axis
+int LYPOS=0;            //position (0-255) of the left joystick y axis
+bool triggers;          //
+PS2X ps2x;              //creating the ps2 controller object
+drivetrain motors;      //creating the motors object
 
 void setup() {
-  motors.intitialize();
+  motors.intitialize(); //attach servos to the pins
   Serial.begin(9600);
-  delay(6000);
-  ps2x.config_gamepad(CLK,CMD,SEL,DAT);
+  delay(6000);          //allows controller to successfuly pair
+  ps2x.config_gamepad(CLK,CMD,SEL,DAT);   //configures the controller
 }
 
 void loop() {
   ps2x.read_gamepad(); //read controller
-  if(ps2x.ButtonPressed(PSB_L2)||ps2x.ButtonPressed(PSB_R2)){
-    if(ps2x.ButtonPressed(PSB_L2))triggers=true;
-    if(ps2x.ButtonPressed(PSB_R2))triggers=false;
-    motors.gripper(triggers);
+  if(ps2x.ButtonPressed(PSB_L2)||ps2x.ButtonPressed(PSB_R2)){     //check to see if either the L2 or R2 trigger have been pulled
+    if(ps2x.ButtonPressed(PSB_L2))triggers=true;                  //check to see if L2 trigger was pulled
+    if(ps2x.ButtonPressed(PSB_R2))triggers=false;                 //check to see if R2 trigger was pulled
+    motors.gripper(triggers);                                     //Closes/opens the gripper
   }
-  RYPOS=ps2x.Analog(PSS_RY);
-  LYPOS=ps2x.Analog(PSS_LY);
-  motors.forward(LYPOS,RYPOS);
+  RYPOS=ps2x.Analog(PSS_RY);                                      //get the Right joystick Y axis value
+  LYPOS=ps2x.Analog(PSS_LY);                                      //get the Left joystick Y axis value
+  motors.forward(LYPOS,RYPOS);                                    //moves servos based on RYPOS and LYPOS
 }
--- code/drivetrain.cpp
+++ code/drivetrain.cpp
@@ -1,61 +1,61 @@
-#include <Arduino.h>
-#include <Servo.h>
-#define LEFTPIN 8
-#define RIGHTPIN 9
-#define upperThreshold 120
-#define lowerThreshold 60
+#include <Arduino.h>          //included the main arduino library
+#include <Servo.h>            //includes the servo library
+#define LEFTPIN 8             //sets the left servvo data pin
+#define RIGHTPIN 9            //sets the right servo data pin
+#define upperThreshold 120    //sets the upper deadzone limit for the joystick
+#define lowerThreshold 60     //set the lower deadzone limit for the joystick
 
 
 class drivetrain{
     private:
-      Servo servoLeft;
-      Servo servoRight;
-      int degreesL;
-      int degreesR;
-      bool closed;
+      Servo servoLeft;        //create a servo object for the left wheel
+      Servo servoRight;       //create a servo object for the right wheel
+      int degreesL;           //initialize the left joystick position variable
+      int degreesR;           //initialize the right joystick position variable
+      bool closed;            //initialize the check to see if the gripper is closed
       int calculateLeft(int yInputL){
-        if(yInputL==255)return 1000;
-        degreesL = map(yInputL, 0,255,180,0);
-        if(degreesL>lowerThreshold&&degreesL<upperThreshold){
-          degreesL = 90;
+        if(yInputL==255)return 1000;            //bandaid fix for Logitech controller potentiometers going bad
+        degreesL = map(yInputL, 0,255,180,0);   //maps the joystick position from 0-255 to 180-0 for easier passing into servo angle
+        if(degreesL>lowerThreshold&&degreesL<upperThreshold){     //checking to see if joystick is in the dead zone
+          degreesL = 90;                                          //if it is then set the angle to 90 (no motion for continuous servo)
         }
-        return degreesL;
+        return degreesL;                                          //returns the angle as an integer
       }
       int calculateRight(int yInputR){        
-        if(yInputR==255)return 1000;
-        degreesR = map(yInputR, 0,255,0,180);
-        if(degreesR>lowerThreshold&&degreesR<upperThreshold){
-          degreesR = 90;
+        if(yInputR==255)return 1000;            //bandaid fix for Logitech controller potentiometers going bad
+        degreesR = map(yInputR, 0,255,0,180);   //maps the joystick position from 0-255 to 0-180 for easier passing into servo angle
+        if(degreesR>lowerThreshold&&degreesR<upperThreshold){     //checking to see if joystick is in the dead zone
+          degreesR = 90;                                          //if it is then set the angle to 90 (no motion for continuous servo)
         }
-        return degreesR;
+        return degreesR;                                          //returns the angle as an integer
       }
     public:
-        void intitialize(){
-          servoLeft.attach(LEFTPIN);
-          servoRight.attach(RIGHTPIN);
+        void intitialize(){                         //set up the servos
+          servoLeft.attach(LEFTPIN);                //attaching the left servo
+          servoRight.attach(RIGHTPIN);              //attaching the right servo
           return;
         }
-        void forward(int Ljoystick,int Rjoystick){
-          int Lspeed = calculateLeft(Ljoystick);
-          int Rspeed = calculateRight(Rjoystick);
-          if(Lspeed!=1000){
-            servoLeft.write(Lspeed);
+        void forward(int Ljoystick,int Rjoystick){  //move the robot 
+          int Lspeed = calculateLeft(Ljoystick);    //getting the angle to write to the left servo
+          int Rspeed = calculateRight(Rjoystick);   //getting the angle to write to the right servo
+          if(Lspeed!=1000){                         //checks to make sure the original value was not 255
+            servoLeft.write(Lspeed);                //if it wasn't, then write calculated angle to the left servo
           }
-          if(Rspeed!=1000){
-            servoRight.write(Rspeed);
+          if(Rspeed!=1000){                         //checks to make sure the original value was not 255
+            servoRight.write(Rspeed);               //if it wasn't, then write calculated angle to the left servo
           } 
         }
-        void gripper(bool mode){
-          if(mode==closed)return;
-          if(mode==true){
+        void gripper(bool mode){                    //Close/Open the gripper
+          if(mode==closed)return;                   //checks to see if the current state of the gripper is what what inputted
+          if(mode==true){                           //L2 has been pressed
             Serial.println("Gripper opened");
-            closed=mode;
+            closed=mode;                            //sets the boolean check to open
             //Open the servo here
           }
-          if(mode==false){
+          if(mode==false){                          //checks to see if the current state of the gripper is what what inputted
             //Close the servo here
             Serial.println("Gripper closed");
-            closed=mode;
+            closed=mode;                            //sets the boolean check to closed
           }
           
         }
